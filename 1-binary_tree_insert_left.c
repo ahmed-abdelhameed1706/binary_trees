410dabb29e25cc61c57488145f87cbd7578da4cc
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-08-28 06:32:50 -0700

    adding 0 and 1 tasks

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,31 @@
+#include "binary_trees.h"
+
+/**
+ * *binary_tree_insert_left - a function to insert a node to
+ * the left of another node
+ * @parent: node to have a node inserted to
+ * @value: value of the new inserted node
+ *
+ * Return: pointer to the new node
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	if (parent == NULL)
+		return (NULL);
+
+	new_node = binary_tree_node(parent, value);
+
+	if (new_node == NULL)
+		return (NULL);
+
+	if (parent->left)
+	{
+		new_node->left = parent->left;
+		parent->left->parent = new_node;
+	}
+	parent->left = new_node;
+
+	return (new_node);
+}
