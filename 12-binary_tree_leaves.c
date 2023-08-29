88f49d61dc61715e2f74ebc755be7f1ed82981e1
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-08-29 09:54:15 +0300

    12-binary_tree_leaves.c

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,19 +8,11 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t leaves = 0;
-
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left)
-		leaves++;
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
 
-	if (tree->right)
-		leaves++;
-
-	if (!tree->left && !tree->right)
-		leaves++;
-
-	return (leaves);
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
