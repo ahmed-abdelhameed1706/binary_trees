5e0ac8522683ff22f2904a9a1c745bf93412da9a
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-08-29 15:45:35 +0300

    binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -26,7 +26,6 @@ typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
 /* Prototypes */
-size_t power(size_t base, size_t exponent);
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
@@ -47,5 +46,5 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
-
+size_t power(size_t base, size_t exponent);
 #endif /* BINARY_TREES_H */
