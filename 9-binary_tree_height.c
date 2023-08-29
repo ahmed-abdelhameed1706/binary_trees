c6ca9cb7f005c6f4e24c7354b9171a4a21c037a8
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-08-29 09:01:27 +0300

    9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,7 +10,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height = 0, right_height = 0;
 
-	if (tree)
+	if (tree != NULL)
 	{
 		if (tree->left)
 			left_height = 1 + binary_tree_height(tree->left);
